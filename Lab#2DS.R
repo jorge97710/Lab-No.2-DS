3ea1ce8653d6c883e5aa30cf3b1d7eb30e5b4b00
jorge97710 <jazmitia_10@hotmail.com>
2019-08-21 12:04:48 -0600

    paso 1

---
+++ Lab#2DS.R
@@ -0,0 +1,54 @@
+sample <- read.csv(file="sample_submission.csv", header=TRUE)
+test <- read.csv(file="test.csv", header=TRUE)
+train <- read.csv(file="train.csv", header=TRUE)
+
+#simple random sample for 60% train and 40% test. based on train 
+porcentaje<-0.6
+
+corte <- sample(nrow(train),nrow(train)*porcentaje)
+train<-train[corte,]
+test<-train[-corte,]
+#poniento uno con solo numericas 
+
+
+cuantitativas<-data.frame(
+  
+  "LotFrontage"=train$LotFrontage,
+  "LotArea"= train$LotArea,
+  "YearBuilt"=train$YearBuilt,
+  "YearRemodAdd"=train$YearRemodAdd,
+  "MasVnrArea"=train$MasVnrArea,
+  "BsmtFinSF1"=train$BsmtFinSF1,
+  "BsmtFinSF2"=train$BsmtFinSF2,
+  "BsmtUnfSF"=train$BsmtUnfSF,
+  "TotalBsmtSF"=train$TotalBsmtSF,
+  "BsmtFinSF1"=train$BsmtFinSF1,
+  "X1stFlrSF"=train$X1stFlrSF,
+  "X2ndFlrSF"=train$X2ndFlrSF, 
+  "GrLivArea"=train$GrLivArea,
+  "BsmtFullBath"=train$BsmtFullBath ,
+  "BsmtHalfBath"=train$BsmtHalfBath,
+  "BedroomAbvGr"=train$BedroomAbvGr,
+  "KitchenAbvGr"=train$KitchenAbvGr, 
+  "TotRmsAbvGrd"=train$TotRmsAbvGrd,
+  "Fireplaces"=train$Fireplaces,
+  "GarageYrBlt"=train$GarageYrBlt,
+  "GarageCars"=train$GarageCars,
+  "GarageArea"=train$GarageArea,
+  "WoodDeckSF"=train$WoodDeckSF,
+  "OpenPorchSF"=train$OpenPorchSF ,
+  "EnclosedPorch"=train$EnclosedPorch,
+  "X3SsnPorch"=train$X3SsnPorch,
+  "ScreenPorch"=train$ScreenPorch,
+  "PoolArea"=train$PoolArea,
+  "MiscVal"=train$MiscVal,
+  "YrSold"=train$YrSold,
+  "SalePrice"=train$SalePrice
+)
+
+#-------------------------------------------------
+# Regresión Lineal Simple 
+#-------------------------------------------------
+
+rls <- lm()
+fitLMPW<-lm(Petal.Length~Petal.Width, data = train)
